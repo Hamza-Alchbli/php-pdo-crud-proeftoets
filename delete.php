8002d214e244cc089e768b5f249867049b727021
Hamza <337118@student.mboutrecht.nl>
2022-12-16 11:49:48 +0100

    commit-6

--- delete.php
+++ delete.php
@@ -7,14 +7,8 @@ try {
     $sql = "delete from RichestPeople where id = $id";
     $pdo->exec($sql);
     echo "Deleted successfully";
-    header('Location: read.php',true, 500);
-    if ($result) {
-        echo "Het record is verwijderd";
-        header('Refresh:3; url=read.php');
-    } else {
-        echo "Het record is niet verwijderd";
-        header('Refresh:3; url=read.php');
-    }
+    header('Refresh:3; url=read.php');
+
 } catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
